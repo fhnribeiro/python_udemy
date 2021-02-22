ef3a4515621eba2fb1c9f64b01df3bde287138d6
fhnribeiro <fhnribeiro@hotmail.com>
2021-02-21 22:17:32 -0300

    My final version

--- app1 - dictionary/app1.py
+++ app1 - dictionary/app1.py
@@ -1,12 +1,30 @@
 import json
+from difflib import get_close_matches
 
-def find_word(dictionary, word):
+dictionary = json.load(open("data.json"))
+
+def find_word(word):
+    word = word.lower()
     if word in dictionary:
-        return "\n".join(dictionary[word])
+        return [1, "\n".join(dictionary[word])]
+    elif word.title() in dictionary:
+        return [1, "\n".join(dictionary[word.title()])]
     else:
-        return "Word not found"
+        closest = get_close_matches(word, dictionary.keys(),1,0.8)
+        if len(closest) > 0:
+            return [2, closest[0]]
+        else:    
+            return [0, "Word not found"]
 
-with open("data.json") as data:
-    content = json.load(data)
-    word = input("Enter the word you want to know")
-    print(find_word(content,word))
+code = 0
+while code != 1:
+    word = input("Enter the word you want to know: ")
+    [code, result] = find_word(word)
+    if(code == 2):
+        confirm = input("Did yoy mean %s? Type Y for yes and N for no: " % result)
+        if(confirm.lower() == "n"):
+            code = 0
+        else:
+            [code, result] = find_word(result)
+    if code == 1:
+        print(result)
